a331781bf25d5d025e12dcf5d9e68751b51843bd
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2025-08-04 21:49:11 +0200

    Add compression middleware to Express app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 const { swaggerUi, specs } = require('./utils/swagger');
+const compression = require('compression')
 
 const app = express();
 
@@ -14,6 +15,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
